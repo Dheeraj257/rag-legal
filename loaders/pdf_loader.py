1ae9c719a9d72aa3b4c76de13c306cb74916ae7a
dheeraj257 <dheerajvuppala9@gmail.com>
2026-04-09 21:28:31 +0530

    upgrade pdf_loader to handle text, scanned and mixed pdf pages with OCR support

--- loaders/pdf_loader.py
+++ loaders/pdf_loader.py
@@ -1,6 +1,42 @@
 import fitz
 from pathlib import Path
 from langchain_core.documents import Document as LCDocument
+from PIL import Image, ImageEnhance
+import pytesseract
+import io
+
+def extract_text_page(page):
+
+    texts = []
+
+    blocks = page.get_text("blocks")
+
+    for block in blocks:
+        x0, y0, x1, y1, content, _, block_type = block
+        if block_type == 0:
+            text = content.strip()
+            texts.append({
+                "content": text,
+                "y_pos": y0
+            })
+    
+    return texts
+
+def extract_ocr_page(image):
+
+    image = image.convert("L")
+
+    contrast_enhancer = ImageEnhance.Contrast(image)
+    image = contrast_enhancer.enhance(2.0)
+
+    sharpness_enhancer = ImageEnhance.Sharpness(image)
+    image = sharpness_enhancer.enhance(2.0)
+
+    ocr_text = pytesseract.image_to_string(
+        image,
+        config= "--psm 3 --oem 1")
+    
+    return ocr_text
 
 
 def load_pdf(file_path):
@@ -10,22 +46,44 @@ def load_pdf(file_path):
 
     with fitz.open(file_path) as pdf:
 
-        for page_num, page in enumerate(pdf.pages()):
+        for page_num, page in enumerate(pdf.pages(),start=1):
 
-            text = pdf.get_page_text(page_num)
+            elements = []
+            text = extract_text_page(page)
+            for block in text:
+                elements.append({
+                    "content": block["content"],
+                    "y_pos": block["y_pos"],
+                    "type": "text"
+                })
 
-            if len(text) < 20:
-                continue
 
-            pdf_docs.append(LCDocument(
-                page_content=text,
-                metadata={
-                    "source": file_name,
-                    "page": page_num,
-                    "citation": f"{file_name}: {page_num}"
-                }
-            ))
+            for img_ref in page.get_images(full=True):
+                xref = img_ref[0]
+                bbox = page.get_image_bbox(img_ref)
+                base_image = pdf.extract_image(xref)
+                pil_image = Image.open(io.BytesIO(base_image["image"]))
+                ocr_text = extract_ocr_page(pil_image)
+                elements.append({
+                    "content": ocr_text,
+                    "y_pos": bbox.y0,
+                    "type": "image"
+                })
 
+            elements.sort(key=lambda x:x["y_pos"])
+
+            for element in elements:
+                if len(element["content"]) < 20:
+                    continue
+                pdf_docs.append(LCDocument(
+                    page_content=element["content"],
+                    metadata={
+                        "source": file_name,
+                        "page": page_num,
+                        "type": element["type"],
+                        "citation": f"{file_name}: {page_num}"
+                    }
+                ))
+  
     return pdf_docs
-
 
